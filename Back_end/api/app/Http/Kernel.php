208f8b1c0ed34f0da67ba0260383170df71e4243
thang <vanthang@gmail.com>
2021-11-02 02:31:23 +0700

    first commit

--- Back_end/api/app/Http/Kernel.php
+++ Back_end/api/app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Session\Middleware\StartSession::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
